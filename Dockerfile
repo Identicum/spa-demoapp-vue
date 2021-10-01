940d0f4974195c58c5c82842d3ec83b93cb0b007
Ivan Michco <imichco@identicum.com>
2021-10-01 19:01:35 -0300

    Work in progress

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,12 @@ RUN npm install
 COPY . .
 RUN npm run build
 
-FROM nginx:stable-alpine as production-stage
-LABEL maintainer="Martin Besozzi <mbesozzi@identicum.com>"
-ARG GIT_SHA1=unspecified
-LABEL git_commit=$GIT_SHA1
-ARG BUILD_DATE=unspecified
-LABEL build_date=$BUILD_DATE
+FROM identicum/ipax:latest
 
-COPY --from=build-stage /app/dist /usr/share/nginx/html
-COPY nginx/default.conf /etc/nginx/conf.d/default.conf
-
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+ENV SESSION_COOKIE_PERSISTENT=off \
+    SESSION_COOKIE_LIFETIME=86400 \
+    API_URL=http://myapi:8080/
+    
+COPY --from=build-stage /app/dist /var/ipax/html
+COPY conf/ /usr/local/openresty/nginx/conf/
+COPY lua/ /etc/ipax/lua/
---
+++ lua/ipax.lua
@@ -0,0 +1,94 @@
+local _M = {}
+
+local oidc_opts = {
+	discovery = os.getenv("OIDC_DISCOVERY"),
+	ssl_verify = "no",
+	client_id = os.getenv("OIDC_CLIENT_ID"),
+	client_secret = os.getenv("OIDC_CLIENT_SECRET"),
+	scope = os.getenv("OIDC_SCOPE"),
+	redirect_uri = os.getenv("OIDC_REDIRECT_URI"),
+	renew_access_token_on_expiry = true,
+	session_contents = {id_token=false, access_token=true, user=true}
+}
+
+local session_opts = {
+	secret = os.getenv("SESSION_SECRET"),
+	cookie         = {
+		persistent = os.getenv("SESSION_COOKIE_PERSISTENT")
+		lifetime   = os.getenv("SESSION_COOKIE_LIFETIME")
+	}
+}
+
+local function split(input, separator)
+	if separator == nil then
+		separator = "%s"
+	end
+	local t={}
+	for str in string.gmatch(input, "([^" .. separator .. "]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
+local function check_authentication(err)
+	if err then
+		ngx.status = 500
+		ngx.say(err)
+		ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+		return false
+	end
+	return true
+end
+
+
+function _M.get_user()
+	local res = _M.get_res()
+	return res.user
+end
+
+function _M.get_id_token()
+	local res = _M.get_res()
+	return res.id_token
+end
+
+function _M.get_access_token()
+	local res = _M.get_res()
+	return res.access_token
+end
+
+function _M.get_res()
+	local res, err, target, session = require("resty.openidc").authenticate(oidc_opts, null, action, session_opts)
+	session:close()
+	local authentication_feedback = check_authentication(err)
+	return res
+end
+
+function _M.check_multivalued_user_claim(claim_values, check_item)
+	for index, value in pairs(claim_values) do
+		-- ToDo: compare case-insensitive
+		if value == check_item then
+			return true
+		end
+	end
+	ngx.exit(ngx.HTTP_FORBIDDEN)
+	return false
+end
+
+function _M.get_group_names(claim_values, separator)
+	if claim_values == nil then
+		ngx.log(ngx.DEBUG, 'claim_values is null.')
+		return ""
+	end
+	if separator == nil then
+		separator = "|"
+	end
+	local group_names = {}
+	for index, value in pairs(claim_values) do
+		local object_rdn = split(value, ",")[1]
+		local object_name = split(object_rdn, "=")[2]
+		group_names[index] = object_name
+	end
+	return table.concat(group_names, separator)
+end
+
+return _M
